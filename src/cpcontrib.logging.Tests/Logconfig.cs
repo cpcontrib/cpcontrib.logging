1f27867ff45697a5db8953f31cce5b4bf11de19a
Eric Newton <ericnewton76@gmail.com>
2018-12-26 14:51:49 -0500

    added Tests class for testing CPLog specific stuff only

---
+++ src/cpcontrib.logging.Tests/Logconfig.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logging_Tests
+{
+	[TestFixture]
+	public class Logconfig
+	{
+		[Test]
+		public void TestMethod()
+		{
+			//CPLog.Config.LoggingConfiguration
+
+			var Log = CPLog.LogManager.GetLogger(typeof(TestClass1));
+
+			
+		}
+
+		public class TestClass1
+		{
+
+		}
+	}
+}
